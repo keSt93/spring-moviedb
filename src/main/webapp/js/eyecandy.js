ac14c8753e609ed86ae7d5e5f26e32525cfdc185
keSt <kevin.stein93@gmail.com>
2018-04-25 19:19:21 +0200

    movieinstance stuff

--- src/main/webapp/js/eyecandy.js
+++ src/main/webapp/js/eyecandy.js
@@ -1,4 +1,30 @@
 // Progress Bar Stuff --------------------------------------------------------------------------------
+function adjustRatingProgressBarColor(elemClass, percentage) {
+    $(elemClass).css('background-color', function(){;
+        if (percentage > 0 && percentage < 15){
+            return '#721414'
+        }
+        else if (percentage > 14 && percentage < 30){
+            return '#a91818'
+        }
+        else if (percentage > 29 && percentage < 45){
+            return '#cb7300'
+        }
+        else if (percentage > 44 && percentage < 60){
+            return '#d19e00'
+        }
+        else if (percentage > 59 && percentage < 75) {
+            return '#bfbc00';
+        }
+        else if (percentage > 74 && percentage <= 90) {
+            return '#7ba01c';
+        }
+        else if (percentage > 89 && percentage <= 100) {
+            return '#3a8d24';
+        }
+    });
+}
+
 function adjustRatingProgressBar(elemClass, percent) {
     $(elemClass).each(function(i) {
         var interval = setInterval(progress, 10);
@@ -15,59 +41,27 @@ function adjustRatingProgressBar(elemClass, percent) {
                 adjustRatingProgressBarColor(elemClass, width);
             }
         }
-
-        function adjustRatingProgressBarColor(elemClass, percentage) {
-            $(elemClass).css('background-color', function(){;
-                if (percentage > 0 && percentage < 25){
-                    return '#a41818'
-                }
-                else if (percentage > 24 && percentage < 50) {
-                    return '#87581c';
-                }
-                else if (percentage > 49 && percentage < 75) {
-                    return '#997815';
-                }
-                else if (percentage > 74 && percentage < 90) {
-                    return '#7ba01c';
-                }
-                else if (percentage > 89 && percentage <= 100) {
-                    return '#3a8d24';
-                }
-            });
-        }
     })
 }
-
-function instantlyAdjustRatingProgressBar(elemClass, percent) {
+function adjustRatingProgressBarWithCustomText(elemClass, percent, customText) {
     $(elemClass).each(function(i) {
+        var interval = setInterval(progress, 10);
+        var width = 1;
         var that = this;
         var maxwidth = percent;
-
-        that.style.width = maxwidth + '%';
-        that.innerHTML = maxwidth + '%';
-        adjustRatingProgressBarColor(elemClass, maxwidth);
-
-        function adjustRatingProgressBarColor(elemClass, percentage) {
-            $(elemClass).css('background-color', function(){;
-                if (percentage > 0 && percentage < 25){
-                    return '#a41818'
-                }
-                else if (percentage > 24 && percentage < 50) {
-                    return '#87581c';
-                }
-                else if (percentage > 49 && percentage < 75) {
-                    return '#997815';
-                }
-                else if (percentage > 74 && percentage < 90) {
-                    return '#7ba01c';
-                }
-                else if (percentage > 89 && percentage <= 100) {
-                    return '#3a8d24';
-                }
-            });
+        function progress() {
+            if (width >= maxwidth) {
+                clearInterval(interval);
+            } else {
+                width++;
+                that.style.width = width + '%';
+                that.innerHTML = customText + width * 1 + '%';
+                adjustRatingProgressBarColor(elemClass, width);
+            }
         }
     })
 }
+
 // End Progress Bar Stuff --------------------------------------------------------------------------------
 
 // Number Stuff --------------------------------------------------------------------------------
