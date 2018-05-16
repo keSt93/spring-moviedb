eab5f7b12fdc8764e1172d545a80e6b585a1415c
keSt <kevin.stein93@gmail.com>
2018-05-16 13:07:56 +0200

    removed register button, cleaned userlogincontroller

--- src/main/java/keSt93/springmoviedb/controller/UserLoginController.java
+++ src/main/java/keSt93/springmoviedb/controller/UserLoginController.java
@@ -28,21 +28,6 @@ public class UserLoginController {
 
     @RequestMapping(value = "/login")
     public String login(Principal principal) {
-        ModelAndView m = new ModelAndView("userLogin");
-
-        // get Notifications from User, if logged in
-        Iterable<NotificationUserRelation> notifications;
-        NotificationHelper notificationHelper = new NotificationHelper(userRepository, notificationUserRelationRepository, notificationTypeRepository);
-        notifications = notificationHelper.getNotificationsFromUser(principal);
-
-        // Calculate wasted Time for Footer
-        int wastedMinutes = movieRepository.getTotalWastedMinutes();
-        int wastedHours = wastedMinutes / 60;
-        wastedMinutes = wastedMinutes % 60;
-
-        m.addObject("wastedMinutes", wastedMinutes);
-        m.addObject("wastedHours", wastedHours);
-        m.addObject("notifications", notifications);
 
         return "userLogin";
     }
